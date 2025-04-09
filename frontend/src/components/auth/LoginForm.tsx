a6a04fb8866a0f7d240591b17f5310bac4204c1a
squach28 <squach3443@gmail.com>
2025-04-09 14:07:56 -0700

    add validation for fields and add error styling

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -2,9 +2,19 @@ import { Box, Button, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { UserData } from "../../types/UserData";
 import { Link } from "react-router";
+import validator from "validator";
+
+type UserDataErrors = {
+  email: string;
+  password: string;
+};
 
 const LoginForm = () => {
   const [userData, setUserData] = useState<UserData>({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState<UserDataErrors>({
     email: "",
     password: "",
   });
@@ -19,6 +29,37 @@ const LoginForm = () => {
         [name]: value,
       };
     });
+  };
+
+  const handleLoginClicked = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const emailError = validateEmail(userData.email);
+    const passwordError = validatePassword(userData.password);
+
+    setErrors({
+      email: emailError,
+      password: passwordError,
+    });
+  };
+
+  const validateEmail = (email: string) => {
+    if (validator.isEmpty(email)) {
+      return "Email is required";
+    }
+
+    if (!validator.isEmail(email)) {
+      return "Email is invalid";
+    }
+
+    return "";
+  };
+
+  const validatePassword = (password: string) => {
+    if (validator.isEmpty(password)) {
+      return "Password is required";
+    }
+
+    return "";
   };
 
   return (
@@ -39,6 +80,12 @@ const LoginForm = () => {
         name="email"
         type="email"
         label="Email"
+        error={errors.email !== ""}
+        helperText={
+          errors.email ? (
+            <Typography color="error">{errors.email}</Typography>
+          ) : null
+        }
         fullWidth
         onChange={onInputChange}
         value={userData.email}
@@ -48,11 +95,24 @@ const LoginForm = () => {
         name="password"
         type="password"
         label="Password"
+        error={errors.password !== ""}
+        helperText={
+          errors.password ? (
+            <Typography color="error">{errors.password}</Typography>
+          ) : null
+        }
         fullWidth
         onChange={onInputChange}
         value={userData.password}
       />
-      <Button variant="contained">Log in</Button>
+      <Button
+        type="submit"
+        variant="contained"
+        onClick={handleLoginClicked}
+        size="large"
+      >
+        Log in
+      </Button>
       <Typography
         variant="body1"
         sx={{
